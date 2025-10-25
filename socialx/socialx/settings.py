53c1cee37402683ce360166a86487b950e577cd5
CodeByNilesh <nj685653@gmail.com>
2025-10-25 14:44:15 +0530

    Updated before deployment

--- socialx/socialx/settings.py
+++ socialx/socialx/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+from dotenv import load_dotenv
 from pathlib import Path
 import os
 
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--!s$a&yjp9qmsv7!qvf=u#_*m^wf$ojeh_s_3)w7$tyim!@^x0'
+load_dotenv()  # loads variables from .env file
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourapp.onrender.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
